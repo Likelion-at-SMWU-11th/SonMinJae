38fb06c89cf8a0c906b0c258f811918333e6fb13
MinJaeSon <mjsonsj@gmail.com>
2023-07-03 22:18:51 +0900

    프론트 1주차 과제

---
+++ front_w01/hello.js
@@ -0,0 +1,13 @@
+const loginForm = document.querySelector("#login-form");   
+const loginInput = document.querySelector("#login-form input");
+const hello = document.querySelector("#hello");
+
+function loggedIn(event){
+    event.preventDefault();
+    loginForm.classList.add("hidden");  // 요소에 "hidden"이라는 이름의 class 추가
+    const userName = loginInput.value;
+    hello.innerText = `Login Completed! Hello ${userName} .`; 
+    hello.classList.remove("hidden");
+}
+
+loginForm.addEventListener("submit", loggedIn);   // "submit" 이벤트 만들기
