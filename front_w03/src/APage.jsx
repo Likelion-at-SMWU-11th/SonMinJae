28a4921ce0bc2356d9887a98de0e1e759c78e413
MinJaeSon <mjsonsj@gmail.com>
2023-07-18 18:46:44 +0900

    프론트 3주차 과제

---
+++ front_w03/src/APage.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const APage = () => {
+    const name = "A페이지"
+
+    return (
+        <div>
+            여기는 {name}입니다.
+        </div>
+    );
+};
+
+export default APage;
